6a0f25b2bc4a4ff759857c6c109331cbffa27d48
aozora <marcellop@protonmail.com>
2024-09-20 15:03:39 +0200

    feat: wip leaderboard

--- src/routes/(app)/leaderboard/+page.server.ts
+++ src/routes/(app)/leaderboard/+page.server.ts
@@ -1,6 +1,7 @@
 import type { PageServerLoad } from './$types';
 import type { Player } from '@prisma/client';
 import { getActiveTournament, getLeaderboard, getPlayers } from '$lib/server/repository';
+import { error } from '@sveltejs/kit';
 
 /**
  * Page Load
@@ -8,6 +9,10 @@ import { getActiveTournament, getLeaderboard, getPlayers } from '$lib/server/rep
 export const load: PageServerLoad = async () => {
 	const tournament = await getActiveTournament();
 	const players: Array<Player> = await getPlayers();
+
+	if (!tournament) {
+		error(500, 'Active tournament not found!');
+	}
 
 	const leaderboard = await getLeaderboard(tournament.id);
 
