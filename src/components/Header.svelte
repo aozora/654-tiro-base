0640b828529d57e631c935d268a8d66995ac4384
aozora <marcellop@protonmail.com>
2024-09-25 12:32:39 +0200

    feat: logout, mobile  menu

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-
 	import { DiceSix } from 'phosphor-svelte';
 	import MobileMenu from '$components/MobileMenu.svelte';
 
@@ -11,7 +10,12 @@
 		<span>654 tiro base</span>
 	</a>
 
-	<button type="button" class="toggle-menu" aria-expanded={isOpen} on:click={()=>isOpen = !isOpen}>
+	<button
+		type="button"
+		class="toggle-menu"
+		aria-expanded={isOpen}
+		on:click={() => (isOpen = !isOpen)}
+	>
 		<DiceSix size="36" weight="fill" />
 	</button>
 	<MobileMenu open={isOpen} />
@@ -19,49 +23,51 @@
 	<nav class="menu">
 		<!--    <ul></ul>-->
 	</nav>
-
 </header>
 
 <style lang="scss">
-  .header {
-    position: sticky;
-    top: 0;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    height: 48px;
-    padding: 0 1rem;
-    background: var(--color-dark) url(/img/frame-mini1.webp) no-repeat center center;
-    background-size: 100%;
-    z-index: 10;
-  }
+	.header {
+		position: sticky;
+		top: 0;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		width: 100%;
+		height: 48px;
+		padding: 0 1rem;
+		background: var(--color-dark) url(/img/frame-mini1.webp) no-repeat center center;
+		background-size: 100%;
+		z-index: 10;
+	}
 
-  .logo {
-    font-family: var(--variable-font-family-brutal);
-    font-size: var(--text-scale-18);
-    text-decoration: none;
-    color: var(--color-black);
-  }
+	.logo {
+		font-family: var(--variable-font-family-brutal);
+		font-size: var(--text-scale-18);
+		text-decoration: none;
+		color: var(--color-black);
+	}
 
-  .toggle-menu {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 36px;
-    height: 36px;
-    padding: 0;
-    border: 0;
-    background-color: transparent;
-    z-index: 101;
-		transition: color .35s ease-in-out;
+	.toggle-menu {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 36px;
+		height: 36px;
+		padding: 0;
+		border: 0;
+		background-color: transparent;
+		z-index: 101;
+		transition:
+			color 0.35s ease-in-out,
+			transform 0.35s ease-in-out;
 
-		&[aria-expanded="true"] {
-      color: var(--color-white);
+		&[aria-expanded='true'] {
+			color: var(--color-white);
+			transform: rotate(360deg);
 		}
-  }
+	}
 
-  .menu {
-    display: none;
-  }
+	.menu {
+		display: none;
+	}
 </style>
--- src/components/MobileMenu.svelte
+++ src/components/MobileMenu.svelte
@@ -14,53 +14,83 @@
 			<li>
 				<a href="/tournaments">Tornei</a>
 			</li>
-			<li class="separator">
+			<!-- <li class="separator">
 				<div />
-			</li>
+			</li> -->
 			<li>
 				<a href="/admin">Amministrazione</a>
+			</li>
+			<li class="logout">
+				<form action="/logout" method="POST">
+					<button type="submit">Esci</button>
+				</form>
 			</li>
 		</ul>
 	</nav>
 </div>
 
 <style lang="scss">
-  .mobile-menu {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
+	.mobile-menu {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
 		padding: 0 1rem 1rem 1rem;
-    background: var(--color-dark);
-    transform: translate3d(100%, 0, 0);
-    transition: transform .5s ease-in-out;
-    z-index: 100;
+		background: var(--color-dark);
+		transform: translate3d(100%, 0, 0);
+		transition: transform 0.5s ease-in-out;
+		z-index: 100;
 
-    &.is-open {
-      transform: translateX(0);
-    }
-
-		h1 {
-      height: 48px;
-			margin: 0 0 3rem 0;
-      color: var(--color-white);
+		&.is-open {
+			transform: translateX(0);
 		}
 
-    ul, li {
-      list-style-type: none;
-    }
+		header {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			height: 48px;
+			margin: 0 0 3rem 0;
+		}
 
-		li{
-			a {
+		h1 {
+			/* height: 48px; */
+			margin: 0;
+			font-family: var(--variable-font-family-default);
+			font-size: var(--text-scale-20);
+			line-height: 1;
+			color: var(--color-white);
+		}
+
+		ul,
+		li {
+			list-style-type: none;
+		}
+
+		ul {
+			display: flex;
+			flex-direction: column;
+			justify-content: flex-start;
+			align-items: flex-start;
+		}
+
+		li {
+			&.logout {
+				margin-top: 4rem;
+			}
+
+			a,
+			button {
+				padding: 0;
+				margin: 0;
+				border: 0;
+				background-color: transparent;
+				font-family: var(--variable-font-family-brand);
 				font-size: 9vw;
 				color: var(--color-white);
 				text-decoration: none;
 			}
 		}
-  }
-
-  .separator {
-
-  }
+	}
 </style>
