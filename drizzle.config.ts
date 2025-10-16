e5c8f83138509fe1ef1e3e3c18c3e3d963aec27d
aozora <marcellop@protonmail.com>
2025-10-16 20:38:51 +0200

    feat: update schema configuration and add default role

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,11 +4,12 @@ import { config } from 'dotenv';
 config();
 
 export default defineConfig({
-	schema: './src/lib/server/db/schema.ts',
+	schema: ['./src/lib/server/database/schema.ts', './src/lib/server/database/auth-schema.ts'],
 	out: './drizzle',
-	dialect: 'postgresql',
+	dialect: 'turso',
 	dbCredentials: {
-		url: process.env.VITE_DATABASE_URL!
+		url: process.env.VITE_DATABASE_URL!,
+		authToken: process.env.VITE_DATABASE_AUTH_TOKEN!,
 	},
 	verbose: true,
 	strict: true
